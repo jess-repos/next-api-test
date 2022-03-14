573e9d96a36ac88ffdd5b6f5e9e6cbed378b3df6
Mark Christian Albinto <markchristianalbinto@yahoo.com>
2022-03-14 17:27:01 +0000

    test

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const routes = [
+    {
+      feedback: '/api/feedback',
+    },
+  ]
+  res.status(200).json({ routes })
 }
